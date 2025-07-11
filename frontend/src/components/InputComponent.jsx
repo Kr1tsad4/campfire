f636628da53b5eaed3676fc4cbb0d8ca1d71044a
Kritsada <141848930+Kr1tsad4@users.noreply.github.com>
2025-07-11 18:30:53 +0700

    Update input component

--- frontend/src/components/InputComponent.jsx
+++ frontend/src/components/InputComponent.jsx
@@ -7,10 +7,11 @@ function InputComponent({ id, placeholder, value, handleInput, width = 64 }) {
         placeholder={placeholder}
         onChange={(e) => handleInput(e.target.value)}
         value={value}
-       className={border border-gray-400 px-2 py-2 rounded-md w-[${width}px]}
+        className={`border border-gray-400 px-2 py-2 rounded-md w-[${width}px]`}
       />
     </div>
   );
 }
 
 export default InputComponent;
+
