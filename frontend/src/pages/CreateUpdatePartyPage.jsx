cfde85c1bfc85be8847ead45f9557af8f793f079
Kritsada <141848930+Kr1tsad4@users.noreply.github.com>
2025-07-13 18:15:49 +0700

    Update and rename CreatePartyPage.jsx to CreateUpdatePartyPage.jsx

---
+++ frontend/src/pages/CreateUpdatePartyPage.jsx
@@ -0,0 +1,142 @@
+import { useNavigationBar } from "../hooks/useNavigationBar";
+import SideNavContainer from "../components/SideNavContainer";
+import Header from "../components/Header";
+import PartyForm from "../components/PartyForm";
+import { useParty } from "../hooks/useParty";
+import { useTags } from "../hooks/useTags";
+import { useUser } from "../hooks/useUser";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+function CreateUpdatePartyPage({ type }) {
+  const { hideNavBar, toggleSideNavBar } = useNavigationBar();
+  const { loginUser, getLoginUser } = useUser();
+  const { id } = useParams();
+
+  const {
+    party,
+    getPartyTagsAndMembersName,
+    partyName,
+    setPartyName,
+    description,
+    setDescription,
+    selectedDate,
+    setSelectedDate,
+    startTime,
+    setStartTime,
+    endTime,
+    setEndTime,
+    createNewParty,
+    updateMyParty,
+  } = useParty();
+  const {
+    baseTags,
+    selectedTags,
+    fetchBaseTags,
+    handleSelectedTag,
+    setBaseTags,
+  } = useTags();
+  const [enableCreateButton, setEnableCreateButton] = useState(false);
+  useEffect(() => {
+    if (partyName.trim() && selectedDate && startTime) {
+      setEnableCreateButton(true);
+    } else {
+      setEnableCreateButton(false);
+    }
+  }, [partyName, selectedDate, startTime]);
+
+  useEffect(() => {
+    getLoginUser();
+  }, []);
+  useEffect(() => {
+    if (id) {
+      getPartyTagsAndMembersName(id);
+    }
+  }, []);
+  return (
+    <div className="flex bg-[#fff7f8] min-h-screen">
+      <SideNavContainer
+        hideNavBar={hideNavBar}
+        toggleSideNavBar={toggleSideNavBar}
+      />
+      <div className="min-w-full">
+        <div className="-ml-[75px]">
+          <Header hideSearchBar={true} hideNavBar={hideNavBar} />
+        </div>
+        {type === "create" && (
+          <PartyForm
+            type={type}
+            hideNavBar={hideNavBar}
+            partyName={partyName}
+            setPartyName={setPartyName}
+            description={description}
+            setDescription={setDescription}
+            selectedDate={selectedDate}
+            setSelectedDate={setSelectedDate}
+            startTime={startTime}
+            setStartTime={setStartTime}
+            endTime={endTime}
+            setEndTime={setEndTime}
+            baseTags={baseTags}
+            createNewParty={() =>
+              createNewParty(
+                {
+                  partyName,
+                  description,
+                  selectedDate,
+                  startTime,
+                  endTime,
+                  selectedTags,
+                },
+                loginUser._id
+              )
+            }
+            fetchBaseTags={fetchBaseTags}
+            handleSelectedTag={handleSelectedTag}
+            enableCreateButton={enableCreateButton}
+            myParty={null}
+          />
+        )}
+
+        {type === "update" && (
+          <PartyForm
+            myParty={party}
+            type={type}
+            hideNavBar={hideNavBar}
+            partyName={partyName}
+            setPartyName={setPartyName}
+            description={description}
+            setDescription={setDescription}
+            selectedDate={selectedDate}
+            setSelectedDate={setSelectedDate}
+            startTime={startTime}
+            setStartTime={setStartTime}
+            endTime={endTime}
+            setEndTime={setEndTime}
+            baseTags={baseTags}
+            updateMyParty={() =>
+              updateMyParty(
+                {
+                  name: partyName,
+                  description,
+                  date: selectedDate,
+                  startTime,
+                  endTime,
+                  tags: selectedTags,
+                },
+                loginUser?._id,
+                id
+              )
+            }
+            fetchBaseTags={fetchBaseTags}
+            handleSelectedTag={handleSelectedTag}
+            loginUser={loginUser}
+            setBaseTags={setBaseTags}
+          />
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default CreateUpdatePartyPage;
