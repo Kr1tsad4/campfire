c9eb42f31d2a990bd1976734b0ca3456056c3004
Kritsada Atchariyapruk <kritsada.gun123@gmail.com>
2025-07-15 17:43:45 +0700

    Update invite popup add search and show search result & show members in lobby page

--- frontend/src/hooks/useUser.js
+++ frontend/src/hooks/useUser.js
@@ -1,12 +1,23 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { getUser } from "../libs/fetchUsersUtils";
+import { API_URL } from "../libs/api";
+import { getPartyById } from "../libs/fetchPartyUtils";
 
 export const useUser = () => {
   const [loginUser, setLoginUser] = useState(null);
   const [isLoadingUser, setIsLoadingUser] = useState(true);
+  const [allUsers, setAllUsers] = useState([]);
+  const [searchResult, setSearchResult] = useState([]);
+
+  const getAllUser = async () => {
+    const users = await getUser(API_URL);
+    setAllUsers(users || []);
+  };
 
   const saveLoginUserSession = (user) => {
     sessionStorage.setItem("user", JSON.stringify(user));
   };
+
   const getLoginUser = () => {
     const user = JSON.parse(sessionStorage.getItem("user"));
     setLoginUser(user);
@@ -17,11 +28,32 @@ export const useUser = () => {
     sessionStorage.removeItem("user");
   };
 
+  const searchUserByName = async (searchValue, partyId) => {
+    if (!searchValue) {
+      setSearchResult([]);
+      return;
+    }
+
+    const party = await getPartyById(API_URL, partyId);
+    const memberIds = party.members.map((member) => member);
+
+    const result = allUsers.filter(
+      (user) =>
+        user.penName.toLowerCase().includes(searchValue.toLowerCase()) &&
+        !memberIds.includes(user._id)
+    );
+
+    setSearchResult(result);
+  };
+
   return {
     loginUser,
     getLoginUser,
     saveLoginUserSession,
     removeLoginUser,
-    isLoadingUser
+    isLoadingUser,
+    searchUserByName,
+    searchResult,
+    getAllUser,
   };
 };
