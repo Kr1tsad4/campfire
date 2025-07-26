addfb530886836030aa60468dd9e3f91901abed3
Kritsada Atchariyapruk <kritsada.gun123@gmail.com>
2025-07-26 13:13:07 +0700

    Update party lobbby

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -25,7 +25,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<LandingPage />} />
-          <Route path="/auth" element={<AuthPage />} />
+          <Route path="/auth" element={<AuthPage loginUser={loginUser}/>} />
           <Route
             path="/home"
             element={
--- frontend/src/components/Form/PartyForm.jsx
+++ frontend/src/components/Form/PartyForm.jsx
@@ -88,6 +88,7 @@ function PartyForm({
             width="500"
             value={partyName}
             handleInput={(e) => setPartyName(e)}
+            maxLength={50}
           />
         </div>
 
